44d046cb93e2e63878ae84f7ad2488d5e47cab26
Devin Pantess <pantessdevin@gmail.com>
2014-01-29 17:55:05 -0500

    Change 1: Updates to a few Project Euler problems Change 2: Cleaning up the repo

--- Problem1.py
+++ Problem1.py
@@ -1,20 +1,46 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 13 16:42:30 2013
+Project Euler Problem #1
+Created on Fri Dec 13 16:42:30 2013 @author: pante_000
 
-@author: pante_000
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-# Project Euler Problem #1
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 
-# and 9. The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+# import timing tools
+import time
+
+# Declare variables
+
+result = 0
+starttime = time.time()
+
+# I use a list comprehension and modulo operator to
+# check for numbers in the range divisible by 3's and 5's
+# store the results in the variable to_sum
+
+to_sum = [x for x in range(1,1000) if x % 3 == 0 or x % 5 == 0]
+
+# read out the list comprehension and sum the
+# values in result by using for loop over the list
+
+for n in to_sum:
+   result += n
+
+print "The answer is", result
+print "Finished in", time.time() - starttime, "seconds"
+
+"""
+My initial "naive" solution follows below:
+
 three = 3
 five = 5
 i = 1
 j = 1
 data = [] # a list
-    
+
 num = input("pick a number: ")
 
 while three*i < num:
@@ -23,14 +49,8 @@ while three*i < num:
 while five*j < num:
     data.append(five*j) # add multiples of 5 < num
     j += 1
-    
+
 print list(set(data)) # a "set" is guaranteed to not have duplicates
 print sum(list(set(data))) # sum up the set
-
-# could have also used modulo solution //
-
-#to_sum = [x for x in range(1,1000) if x % 3 == 0 or x % 5 == 0]
-#result = 0
-#for n in to_sum:
-#    result += n
+"""
     
--- Problem3.py
+++ Problem3.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
 """
-Project Euler #3
+Project Euler Problem #3
+Created on Fri Dec 13 16:42:30 2013 @author: pante_000
 
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 
 """
-# prime factorization algorithm
-def factor(x): # naive prime factorization algorithm
+# My naive prime factorization algorithm in function factor(int)
+# utilizes Sieve of Eratosthenes
+def factor(x):
+
     factors = []
+
     i = 2
+
     while x > 1:
+
         if x % i == 0:
+
             x = x / i
+
             factors.append(i)
-        else: 
+
+        else:
+
             i += 1
+
     return factors
 
 num = input("Input an integer: ")
+
 print factor(num)
